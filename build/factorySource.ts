0ba1e3b76af305ff795684aa285f9965b068a5dd
karneges <karneges2012@gmail.com>
2023-08-25 15:31:58 +0900

    final

--- build/factorySource.ts
+++ build/factorySource.ts
@@ -1,11 +1,17 @@
+const factoryAbi = {"ABIversion":2,"version":"2.2","header":["time"],"functions":[{"name":"constructor","inputs":[],"outputs":[]},{"name":"deployAccountForUser","inputs":[],"outputs":[]}],"data":[{"key":1,"name":"accountCode","type":"cell"}],"events":[{"name":"AccountDeployed","inputs":[{"name":"accountAddress","type":"address"},{"name":"owner","type":"address"}],"outputs":[]}],"fields":[{"name":"_pubkey","type":"uint256"},{"name":"_timestamp","type":"uint64"},{"name":"_constructorFlag","type":"bool"},{"name":"MIN_BALANCE","type":"uint128"},{"name":"accountCode","type":"cell"}]} as const
+const factoryAccountAbi = {"ABIversion":2,"version":"2.2","header":["time"],"functions":[{"name":"constructor","inputs":[],"outputs":[]},{"name":"getDetails","inputs":[],"outputs":[{"name":"value0","type":"address"}]},{"name":"owner","inputs":[],"outputs":[{"name":"owner","type":"address"}]},{"name":"factory","inputs":[],"outputs":[{"name":"factory","type":"address"}]}],"data":[{"key":1,"name":"owner","type":"address"},{"key":2,"name":"factory","type":"address"}],"events":[],"fields":[{"name":"_pubkey","type":"uint256"},{"name":"_timestamp","type":"uint64"},{"name":"_constructorFlag","type":"bool"},{"name":"owner","type":"address"},{"name":"factory","type":"address"}]} as const
 const fibonacciAbi = {"ABIversion":2,"version":"2.2","header":["pubkey","time","expire"],"functions":[{"name":"constructor","inputs":[],"outputs":[]},{"name":"setNumber","inputs":[{"name":"_newNumber","type":"uint256"}],"outputs":[]},{"name":"getDetails","inputs":[],"outputs":[{"name":"_firstNumber","type":"uint256"},{"name":"_secondNumber","type":"uint256"}]},{"name":"calculateNextNumber","inputs":[],"outputs":[{"name":"_nextNumber","type":"uint256"}]}],"data":[{"key":1,"name":"_nonce","type":"uint32"}],"events":[{"name":"StateChange","inputs":[{"name":"firstNumber","type":"uint256"},{"name":"secondNumber","type":"uint256"}],"outputs":[]}],"fields":[{"name":"_pubkey","type":"uint256"},{"name":"_timestamp","type":"uint64"},{"name":"_constructorFlag","type":"bool"},{"name":"_nonce","type":"uint32"},{"name":"firstNumber","type":"uint256"},{"name":"secondNumber","type":"uint256"}]} as const
 const simpleWalletAbi = {"ABIversion":2,"version":"2.2","header":["pubkey","time","expire"],"functions":[{"name":"constructor","inputs":[{"name":"_pubKey","type":"uint256"}],"outputs":[]},{"name":"sendToMyFriend","inputs":[{"name":"dest","type":"address"},{"name":"value","type":"uint128"}],"outputs":[]},{"name":"pubKey","inputs":[],"outputs":[{"name":"pubKey","type":"uint256"}]},{"name":"balance","inputs":[],"outputs":[{"name":"balance","type":"uint128"}]}],"data":[],"events":[{"name":"BalanceChanged","inputs":[{"name":"oldBalance","type":"uint128"},{"name":"newBalance","type":"uint128"}],"outputs":[]}],"fields":[{"name":"_pubkey","type":"uint256"},{"name":"_timestamp","type":"uint64"},{"name":"_constructorFlag","type":"bool"},{"name":"pubKey","type":"uint256"},{"name":"balance","type":"uint128"}]} as const
 
 export const factorySource = {
+    Factory: factoryAbi,
+    FactoryAccount: factoryAccountAbi,
     Fibonacci: fibonacciAbi,
     SimpleWallet: simpleWalletAbi
 } as const
 
 export type FactorySource = typeof factorySource
+export type FactoryAbi = typeof factoryAbi
+export type FactoryAccountAbi = typeof factoryAccountAbi
 export type FibonacciAbi = typeof fibonacciAbi
 export type SimpleWalletAbi = typeof simpleWalletAbi
--- test/sample-test.ts
+++ test/sample-test.ts
@@ -1,9 +1,9 @@
 import { expect } from "chai";
 import { Contract, Signer, toNano, WalletTypes } from "locklift";
-import { FactorySource } from "../build/factorySource";
+import { FactoryAccountAbi, FactorySource } from "../build/factorySource";
 import { Account } from "everscale-standalone-client";
 
-let sample: Contract<FactorySource["Fibonacci"]>;
+let accountFactory: Contract<FactorySource["Factory"]>;
 let signer: Signer;
 let user: Account;
 describe("Test Sample contract", async function () {
@@ -13,16 +13,17 @@ describe("Test Sample contract", async function () {
   describe("Contracts", async function () {
     it("Deploy contract", async function () {
       const INIT_STATE = 0;
+      const { code: factoryAccountCode } = locklift.factory.getContractArtifacts("FactoryAccount");
       const { contract } = await locklift.factory.deployContract({
-        contract: "Fibonacci",
+        contract: "Factory",
         publicKey: signer.publicKey,
         initParams: {
-          _nonce: locklift.utils.getRandomNonce(),
+          accountCode: factoryAccountCode,
         },
         constructorParams: {},
         value: locklift.utils.toNano(2),
       });
-      sample = contract;
+      accountFactory = contract;
       user = await locklift.factory.accounts
         .addNewAccount({
           type: WalletTypes.EverWallet,
@@ -30,21 +31,29 @@ describe("Test Sample contract", async function () {
           publicKey: signer.publicKey,
         })
         .then(res => res.account);
-      expect(await locklift.provider.getBalance(sample.address).then(balance => Number(balance))).to.be.above(0);
+      expect(await locklift.provider.getBalance(accountFactory.address).then(balance => Number(balance))).to.be.above(
+        0,
+      );
     });
 
     it("Interact with contract", async function () {
-      const NEW_STATE = 1;
-      const nextNumber = await sample.methods
-        .calculateNextNumber()
-        .call()
-        .then(res => res._nextNumber);
-      const a = await sample.methods
-        .setNumber({ _newNumber: nextNumber })
-        .sendExternal({ publicKey: signer.publicKey });
+      const { traceTree } = await locklift.tracing.trace(
+        accountFactory.methods.deployAccountForUser().send({
+          from: user.address,
+          amount: toNano(2),
+        }),
+      );
+      await traceTree?.beautyPrint();
 
-      const response = await sample.methods.getDetails({}).call();
-      console.log(response);
+      const event = traceTree?.findEventsForContract({
+        contract: accountFactory,
+        name: "AccountDeployed" as const,
+      })[0]!;
+      const { accountAddress } = event;
+      const factoryAccount = locklift.factory.getDeployedContract("FactoryAccount", accountAddress);
+      const details = await factoryAccount.methods.getDetails().call();
+      console.log(`Caller address ${user.address.toString()}`);
+      console.log(`Deployed account address ${details.value0.toString()}`);
     });
   });
 });
